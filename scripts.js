7a55cf615b11c74cb316b2b2461a0d402c63e69e
Vishal <apple@APPLEs-MacBook-Air.local>
2023-12-26 22:45:04 +0530

    section-1.2 Threejs Transformation

--- scripts.js
+++ scripts.js
@@ -6,8 +6,17 @@ const material = new THREE.MeshBasicMaterial({ color: "purple" });
 
 //Mesh (Object)
 const mesh = new THREE.Mesh(geomentry, material);
+mesh.position.x = 1; // place/position the object 1 units from origin in x-axis
+mesh.position.y = 2; // place/position the object 2 units from origin in y-axis
+mesh.position.z = 1; // place/position the object 1 units from origin in z-axis
+mesh.scale.x = 2; // strech the object 2 units on x-axis
+mesh.rotation.x = Math.PI * 0.25; // rotate the object on x-axis
+mesh.rotation.y = Math.PI * 1.25; // rotate the object on y-axis
 
 scene.add(mesh);
+
+const axes = new THREE.AxesHelper(10); // it will show the x,y,z axis on the canvas
+scene.add(axes); //adding the axes to canvas
 
 //Camera
 const aspect = {
@@ -16,7 +25,7 @@ const aspect = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height); // default near value is 1,and far value is 2000
-camera.position.z = 3;
+camera.position.z = 5;
 camera.position.x = 1;
 camera.position.y = 1;
 scene.add(camera);
