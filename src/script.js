608869310264b1c301bc3cf2e034bbc03a422289
Vishal <apple@APPLEs-MacBook-Air.local>
2023-12-28 02:14:17 +0530

    section-1.7 Three.js Particles

--- src/script.js
+++ src/script.js
@@ -4,33 +4,15 @@ import {
   MapControls,
   OrbitControls,
 } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "dat.gui";
 
 //Scene
 const scene = new THREE.Scene();
+const texture = new THREE.TextureLoader();
+const particleTexture = texture.load("/textures/alphaSnow.jpg");
 
-//Lights
-const ambientLights = new THREE.AmbientLight(0xffffff, 0.5);
-const pointLights = new THREE.PointLight(0xffffff, 0.5);
-pointLights.position.set(2, 2, 2);
-scene.add(ambientLights, pointLights);
-
-//texture loader
-const textureLoader = new THREE.TextureLoader();
-const colorTexture = textureLoader.load("/texture/color.jpg");
-const matTexture = textureLoader.load("/texture/mat2.png");
-const bumpTexture = textureLoader.load("/texture/bump.jpg");
-
-//cube texture loader
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const envTexture = cubeTextureLoader.load([
-  "/texture/env/px.png",
-  "/texture/env/nx.png",
-  "/texture/env/py.png",
-  "/texture/env/ny.png",
-  "/texture/env/pz.png",
-  "/texture/env/nz.png",
-]);
-scene.background = envTexture;
+//Debugging
+// const gui = new dat.GUI();
 
 //Resizing
 window.addEventListener("resize", () => {
@@ -47,46 +29,48 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-//Mesh
-const geometry = new THREE.SphereBufferGeometry(0.5, 32, 32);
-// const geometry = new THREE.TorusBufferGeometry(0.3, 0.2, 32, 32);
-// const material = new THREE.MeshBasicMaterial({
-//   map: colorTexture,
-// });
-const material = new THREE.MeshStandardMaterial();
-// material.map = colorTexture;
-// material.bumpMap = bumpTexture;
-// const matMaterial = new THREE.MeshMatcapMaterial({
-//   matcap: matTexture,
-// });
-// material.color = new THREE.Color("red");
-// material.wireframe = true;
-// material.opacity = 0.5;
-// material.transparent = true;
-material.side = THREE.DoubleSide;
-material.roughness = 0.1;
-material.metalness = 0.9;
-material.envMap = envTexture;
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+//Points
+const geometry = new THREE.BufferGeometry();
+const verticesAmount = 10000;
+const positionArray = new Float32Array(verticesAmount * 3);
+for (let i = 0; i < verticesAmount * 3; i++) {
+  positionArray[i] = (Math.random() - 0.5) * 4;
+}
+geometry.setAttribute("position", new THREE.BufferAttribute(positionArray, 3));
+const material = new THREE.PointsMaterial();
+material.size = 0.02;
+material.transparent = true;
+material.alphaMap = particleTexture;
+material.depthTest = true;
+const points = new THREE.Points(geometry, material);
+scene.add(points);
 
 //Camera
 const aspect = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
-camera.position.z = 1;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  aspect.width / aspect.height,
+  0.01,
+  100
+);
+camera.position.z = 2;
 scene.add(camera);
 
 //Renderer
 const canvas = document.querySelector(".draw");
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 renderer.setSize(aspect.width, aspect.height);
 
 //OrbitControls
 const orbitControls = new OrbitControls(camera, canvas);
 orbitControls.enableDamping = true;
+orbitControls.enableZoom = false;
+orbitControls.enableRotate = false;
+orbitControls.autoRotate = true;
+orbitControls.autoRotateSpeed = 0.2;
 
 //Clock Class
 const clock = new THREE.Clock();
@@ -94,10 +78,8 @@ const clock = new THREE.Clock();
 const animate = () => {
   //GetElapsedTime
   const elapsedTime = clock.getElapsedTime();
-  // mesh.rotation.x += 0.1;
-  // mesh.rotation.y += 0.1;
-  // mesh.rotation.z += 0.1;
-
+  // points.rotation.x = elapsedTime * 0.05;
+  // points.rotation.y = elapsedTime * 0.05;
   //Update Controls
   orbitControls.update();
 
