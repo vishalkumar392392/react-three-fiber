4b2e71ac67a374778e06c31092f4719f515b9d75
Vishal <apple@APPLEs-MacBook-Air.local>
2023-12-28 00:53:12 +0530

    section-1.6 Threejs Geomentry

--- src/script.js
+++ src/script.js
@@ -8,6 +8,30 @@ import {
 //Scene
 const scene = new THREE.Scene();
 
+//Lights
+const ambientLights = new THREE.AmbientLight(0xffffff, 0.5);
+const pointLights = new THREE.PointLight(0xffffff, 0.5);
+pointLights.position.set(2, 2, 2);
+scene.add(ambientLights, pointLights);
+
+//texture loader
+const textureLoader = new THREE.TextureLoader();
+const colorTexture = textureLoader.load("/texture/color.jpg");
+const matTexture = textureLoader.load("/texture/mat2.png");
+const bumpTexture = textureLoader.load("/texture/bump.jpg");
+
+//cube texture loader
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const envTexture = cubeTextureLoader.load([
+  "/texture/env/px.png",
+  "/texture/env/nx.png",
+  "/texture/env/py.png",
+  "/texture/env/ny.png",
+  "/texture/env/pz.png",
+  "/texture/env/nz.png",
+]);
+scene.background = envTexture;
+
 //Resizing
 window.addEventListener("resize", () => {
   //Update Size
@@ -24,15 +48,25 @@ window.addEventListener("resize", () => {
 });
 
 //Mesh
-// const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 2, 2, 2);
-const geometry = new THREE.BufferGeometry();
-const verticesArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
-const positionsAttribute = new THREE.BufferAttribute(verticesArray, 3);
-geometry.setAttribute("position", positionsAttribute);
-const material = new THREE.MeshBasicMaterial({
-  color: "purple",
-  wireframe: true,
-});
+const geometry = new THREE.SphereBufferGeometry(0.5, 32, 32);
+// const geometry = new THREE.TorusBufferGeometry(0.3, 0.2, 32, 32);
+// const material = new THREE.MeshBasicMaterial({
+//   map: colorTexture,
+// });
+const material = new THREE.MeshStandardMaterial();
+// material.map = colorTexture;
+// material.bumpMap = bumpTexture;
+// const matMaterial = new THREE.MeshMatcapMaterial({
+//   matcap: matTexture,
+// });
+// material.color = new THREE.Color("red");
+// material.wireframe = true;
+// material.opacity = 0.5;
+// material.transparent = true;
+material.side = THREE.DoubleSide;
+material.roughness = 0.1;
+material.metalness = 0.9;
+material.envMap = envTexture;
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -42,8 +76,7 @@ const aspect = {
   height: window.innerHeight,
 };
 const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
-camera.position.set(2, 2, 2);
-camera.lookAt(mesh.position);
+camera.position.z = 1;
 scene.add(camera);
 
 //Renderer
@@ -61,6 +94,9 @@ const clock = new THREE.Clock();
 const animate = () => {
   //GetElapsedTime
   const elapsedTime = clock.getElapsedTime();
+  // mesh.rotation.x += 0.1;
+  // mesh.rotation.y += 0.1;
+  // mesh.rotation.z += 0.1;
 
   //Update Controls
   orbitControls.update();
